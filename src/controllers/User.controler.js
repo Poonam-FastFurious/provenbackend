d6bbcb10dc12cc0752723288945a838f998ee3f8
Poonam-FastFurious <Poonam.brandbell@gmail.com>
2024-06-10 23:06:34 +0530

    Update User.controler.js

--- src/controllers/User.controler.js
+++ src/controllers/User.controler.js
@@ -118,7 +118,7 @@ const loginUser = asyncHandler(async (req, res) => {
     httpOnly: true,
      httpOnly: true,
     secure: process.env.NODE_ENV === 'production', // Only use secure cookies in production
-    sameSite: 'Lax', // Adjust based on your ne
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',// Adjust based on your ne
   };
 
   return res
