01fa53e169a097692d08504cfd3ef303ed479832
Poonam-FastFurious <Poonam.brandbell@gmail.com>
2024-06-11 00:01:08 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,14 +3,10 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-
-const corsOptions = {
-  origin: process.env.CORS_ORIGIN ,
-  credentials: true,
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
