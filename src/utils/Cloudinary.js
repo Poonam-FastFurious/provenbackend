2859bc6a82886b6dde33da6f228662f1d161de0f
rahulkumar <rahulkumarofficial36@gmail.com>
2024-07-29 13:46:20 +0530

    update

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -1,13 +1,11 @@
-import { v2 as cloudinary } from "cloudinary"
-import fs from "fs"
-import 'dotenv/config'
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import "dotenv/config";
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-
 
 const uploadOnCloudinary = async (localFilePath) => {
   try {
@@ -15,11 +13,9 @@ const uploadOnCloudinary = async (localFilePath) => {
       throw new Error("Local file path is missing");
     }
 
-
-
     // Upload the file to Cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
-      resource_type: "auto"
+      resource_type: "auto",
     });
 
     return response;
@@ -40,6 +36,3 @@ const uploadOnCloudinary = async (localFilePath) => {
 };
 
 export { uploadOnCloudinary };
-
-
-
