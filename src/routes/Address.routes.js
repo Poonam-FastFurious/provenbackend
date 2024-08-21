1f1ddb810410d14f419a4c764ed28596a318f9e5
rahulkumar <rahulkumarofficial36@gmail.com>
2024-08-21 12:43:58 +0530

    update address

--- src/routes/Address.routes.js
+++ src/routes/Address.routes.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
-import { createAddress, getAddresses, updateAddress } from "../controllers/Address.controler.js";
+import {
+  createAddress,
+  deleteAddress,
+  getAddresses,
+  updateAddress,
+} from "../controllers/Address.controler.js";
 import { verifyJWT } from "../middlewares/auth.middlwares.js";
-
-
 
 const router = Router();
 
 router.route("/add").post(verifyJWT, createAddress);
 router.route("/").get(verifyJWT, getAddresses);
 router.route("/update").patch(verifyJWT, updateAddress);
-
+router.route("/delete").delete(verifyJWT, deleteAddress);
 export default router;
