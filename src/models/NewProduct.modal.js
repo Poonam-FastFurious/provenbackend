8fac4031d9ef5df4c3b75d93c6f06a032d28e0af
rahulkumar <rahulkumarofficial36@gmail.com>
2024-08-30 11:15:09 +0530

    update youtube url validate

--- src/models/NewProduct.modal.js
+++ src/models/NewProduct.modal.js
@@ -1,79 +1,78 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema(
-      {
-            title: {
-                  type: String,
-                  required: true,
-                  trim: true,
-            },
-            description: {
-                  type: String,
-                  required: true,
-                  trim: true,
-            },
-            image: {
-                  type: String,
-                  required: true,
-                  trim: true,
-            },
-            stocks: {
-                  type: Number,
-                  required: true,
-                  min: 0,
-            },
-            price: {
-                  type: Number,
-                  required: true,
-                  min: 0,
-            },
-            discount: {
-                  type: String,
-                  trim: true,
-            },
-            cutPrice: {
-                  type: Number,
-                  min: 0,
-            },
-            categories:
-            {
-                  type: String,
-                  trim: true,
-            },
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    image: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    stocks: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    discount: {
+      type: String,
+      trim: true,
+    },
+    cutPrice: {
+      type: Number,
+      min: 0,
+    },
+    categories: {
+      type: String,
+      trim: true,
+    },
 
-            tags: [
-                  {
-                        type: String,
-                        trim: true,
-                  },
-            ],
-            sku: {
-                  type: String,
-                  unique: true,
-                  trim: true,
-            },
-            shortDescription: {
-                  type: String,
-                  trim: true,
-            },
-            youtubeVideoLink: {
-                  type: String,
-                  trim: true,
-                  validate: {
-                        validator: function (v) {
-                              return /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=[\w-]{11}$/.test(v);
-                        },
-                        message: props => `${props.value} is not a valid YouTube URL`
-                  },
-            },
-            thumbnail: [
-                  {
-                        type: String,
-                        trim: true,
-                  },
-            ],
+    tags: [
+      {
+        type: String,
+        trim: true,
       },
-      { timestamps: true }
+    ],
+    sku: {
+      type: String,
+      unique: true,
+      trim: true,
+    },
+    shortDescription: {
+      type: String,
+      trim: true,
+    },
+    youtubeVideoLink: {
+      type: String,
+      trim: true,
+      validate: {
+        validator: function (v) {
+          return /^(https?:\/\/)?(www\.)?youtube\.com\/watch/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid YouTube URL`,
+      },
+    },
+    thumbnail: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+  },
+  { timestamps: true }
 );
 
 export const Product = mongoose.model("Product", productSchema);
